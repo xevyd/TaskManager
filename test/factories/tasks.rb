10cc7a25087eb840f78e3fb2b6ba49e06262a353
Alexey Chumnov <ss.moonlightpath@gmail.com>
2020-05-07 18:35:04 +0400

    add traits

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -2,9 +2,15 @@ FactoryBot.define do
   factory :task do
     name
     description
-    author_id
-    assignee_id
     state
     expired_at
+    
+    trait :author_id do
+      author_id
+    end
+    
+    trait :assignee_id do
+      assignee_id
+    end
   end
 end
