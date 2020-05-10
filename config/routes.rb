0709a8ad7e6e57ac9a6249e2825c8cef18288889
Alexey Chumnov <ss.moonlightpath@gmail.com>
2020-05-10 19:10:42 +0400

    add web controller test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root :to => "web/boards#show"
-
+  
   scope module: :web do
     resource :board, only: :show
-    resource :session, only: :new
+    resource :session, only: [:new, :create, :destroy]
   end
 end
