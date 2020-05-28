0688dabc60ca6fc9123c627665c1bf34ab4b822b
Alexey Chumnov <ss.moonlightpath@gmail.com>
2020-05-28 15:31:08 +0400

    drag bugfix

--- app/javascript/repositories/TasksRepository.js
+++ app/javascript/repositories/TasksRepository.js
@@ -12,9 +12,9 @@ export default {
     return FetchHelper.get(path);
   },
 
-  update(id, task = {}) {
+  update(id, task) {
     const path = routes.apiV1TaskPath(id);
-    return FetchHelper.put(path, task);
+    return FetchHelper.put(path, { task });
   },
 
   create(task = {}) {
